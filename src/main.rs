3c0af158160a8e6d99da82d452837ad98c3d028a
Akshola00 <akinniyishola07@gmail.com>
2025-08-11 13:32:42 +0100

    chore: uncomment i sniffer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 mod concurrency;
 mod ip_sniffer;
 fn main() {
-    // ip_sniffer::ip_sniffer();
+    ip_sniffer::ip_sniffer();
     // concurrency::channel_impl();
-    for i in 0..4 {
-        println!("{i}");
-    }
+
 }
